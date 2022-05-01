a448b4f78738927d422669aadfb640036919af2c
ruegue <95623005+ruegue@users.noreply.github.com>
2022-05-01 15:00:13 +0900

    내적
    통과

---
+++ Solution9.java
@@ -0,0 +1,25 @@
+package level1;
+
+public class Solution9 {
+	
+	public static int solution(int[] a, int[] b) {
+		int answer = 0;
+		
+		for(int i = 0; i < a.length ; i++) {
+			answer += a[i]*b[i];
+		}
+		
+		return answer;
+		
+	}
+
+	public static void main(String[] args) {
+		int[] a = {1,2,3,4};
+		int[] b = {-3,-1,0,2};
+		
+		int sol = solution(a ,b);
+		
+		System.out.println(sol);
+	}
+
+}
